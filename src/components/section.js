2428e7547c2b0108dc72ec8c5a35f174b138dffb
Richard Bacon <richardjohnbacon@gmail.com>
2021-02-16 16:27:52 +0000

    Navigation links scroll to content correctly

--- src/components/section.js
+++ src/components/section.js
@@ -3,9 +3,12 @@ import styles from './section.module.css';
 
 const Section = ({ children, id, heading }) => {
   return (
-    <section id={id} className={styles.section}>
+    <section className={styles.section}>
       <div className={styles.container}>
-        <h2 className={styles.heading}>{heading}</h2>
+        <h2 className={styles.heading}>
+          <span id={id}></span>
+          {heading}
+        </h2>
         {children}
       </div>
     </section>
